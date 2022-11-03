de9a0233d48e2086758e1781bb95f0d89ee5c35b
Kolosecek <gajdos.filip132@gmail.com>
2022-11-03 16:15:28 +0100

    Initial commit

---
+++ src/test/java/com/example/springio/SpringIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
