94ec59477b6fdf3a5b41d3a97a3ce404a8d60a6e
luxao <lukas.lobll@gmail.com>
2022-11-05 15:22:08 +0100

    Add Delete and Update students

--- src/main/java/sk/stuba/fei/asos/asos_project/controllers/StudentThymeleafController.java
+++ src/main/java/sk/stuba/fei/asos/asos_project/controllers/StudentThymeleafController.java
@@ -3,12 +3,11 @@ package sk.stuba.fei.asos.asos_project.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import sk.stuba.fei.asos.asos_project.service.StudentService;
 import sk.stuba.fei.asos.asos_project.student.Student;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/student")
@@ -40,6 +39,21 @@ public class StudentThymeleafController {
     public String addSubmit(@ModelAttribute("student") Student student){
         studentService.saveStudent(student);
         return "redirect:/student/all";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String removeStudent(@PathVariable("id") String id, Model model) {
+        studentService.deleteStudentById(id);
+        model.addAttribute("students", studentService.getAllStudents());
+        return "student/all";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editStudent(@PathVariable("id") Optional<String> id, Model model) {
+        Student student = id.isPresent() ?
+                studentService.findStudentById(id.get()).get() : new Student();
+        model.addAttribute("student", student);
+        return "student/edit";
     }
 
 
